54bbbef2927eb88a1c1de32943536755ea000a6d
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2024-03-27 12:51:58 +0100

    pushing logout functionality production fix v1

--- tuniauctions-web/src/app/api/bidder/signout/route.ts
+++ tuniauctions-web/src/app/api/bidder/signout/route.ts
@@ -4,9 +4,15 @@ export async function GET() {
   const response = NextResponse.json({ success: true });
   response.cookies.set("refreshBidderToken", "", {
     expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
   });
   response.cookies.set("accessBidderToken", "", {
     expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
   });
   return response;
 }
