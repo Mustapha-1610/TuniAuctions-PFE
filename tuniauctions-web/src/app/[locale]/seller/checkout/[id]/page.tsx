a9e4274faf884dc8decc2493592e28f0b642cc1d
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2024-03-31 05:26:35 +0100

    pricing page fetch production fix v1

--- tuniauctions-web/src/app/[locale]/seller/checkout/[id]/page.tsx
+++ tuniauctions-web/src/app/[locale]/seller/checkout/[id]/page.tsx
@@ -3,10 +3,13 @@ import CheckoutPage from "./checkoutPage";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  const res = await fetch("http://localhost:3000/api/utils/getPricing", {
-    method: "POST",
-    body: JSON.stringify({ id }),
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL!}/api/utils/getPricing`,
+    {
+      method: "POST",
+      body: JSON.stringify({ id }),
+    }
+  );
   const resData: getPricingResponse = await res.json();
   if (resData.success) return <CheckoutPage pricing={resData.pricing} />;
   return "No Data";
--- tuniauctions-web/src/app/[locale]/seller/pricing/page.tsx
+++ tuniauctions-web/src/app/[locale]/seller/pricing/page.tsx
@@ -1,10 +1,17 @@
 import { PricingResponse } from "@/app/api/utils/getPricings/route";
 import Pricing from "./PricingPage";
-export default async function Page() {
-  const data = await fetch("http://localhost:3000/api/utils/getPricings", {
-    method: "GET",
-    cache: "no-cache",
-  });
+async function fetchPricingData() {
+  const data = await fetch(
+    `https://tuniauctions.vercel.app/api/utils/getPricings`,
+    {
+      method: "GET",
+      cache: "no-cache",
+    }
+  );
   const resData: PricingResponse = await data.json();
+  return resData;
+}
+export default async function Page() {
+  const resData: PricingResponse = await fetchPricingData();
   return <Pricing pricing={resData.pricings} />;
 }
